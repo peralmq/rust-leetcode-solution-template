674eeccfe16f58918bf7bf56377cebefc3e8104c
Pelle Frank <per.almquist@gmail.com>
2023-08-23 21:07:37 +0200

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+pub struct Solution {}
+
+impl Solution {
+    
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(
+            Solution::
+        );
+    }
+}
